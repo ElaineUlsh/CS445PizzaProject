d0d85e1db9790a9291377aa19d9c08af3ca4ec85
Elaine Ulsh <elaine.ulsh@bsu.edu>
2024-04-04 10:45:49 -0400

    Create App_Code folder and CustomerUtil.cs file.

---
+++ bsuPizza/App_Code/CustomerUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace bsuPizza.App_Code
+{
+    public class CustomerUtil
+    {
+        public String UserName { get; set; }
+
+        public String FName {  get; set; }
+    } // Close CustomerUtil class
+} // Close bsuPizza.App_Code
